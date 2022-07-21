4ef3438c206fafbb4a35053d26a39bda42e41e6a
‘jesus’ <jesersu@gmail.com>
2022-07-20 22:39:22 -0500

    server get test

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,50 @@ mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
         Logging.info('Connected to mongoDB.');
+        StartServer();
     })
     .catch((error) => {
         Logging.error('Unable to connect');
         Logging.error(error);
     });
+
+/** Iniciamos el servidor solo si se conecta Mongo */
+
+const StartServer = () => {
+    router.use((req, res, next) => {
+        /**chequeamos los datos de entrada */
+        Logging.info(`Incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+        res.on('finish', () => {
+            /** respuesta a dar */
+            Logging.info(`Incomming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status [${res.statusCode}]`);
+        });
+        next();
+    });
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    /** Reglas para nuestra API */
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Methods', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /** Rutas */
+
+    /** Healthcheck */
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
+    /**Error */
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port} .`));
+};
